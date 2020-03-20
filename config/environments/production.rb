3f0a06c8f3b4d8ee17fdd0e10656f3a0861dbc17
EC2 Default User <ec2-user@ip-172-31-36-14.ap-northeast-1.compute.internal>
2020-03-20 06:33:39 +0000

    Add password reset mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # See: https://railstutorial.jp/chapters/account_activation?version=5.1#sec-account_activations_resourcea
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'sleepy-hollows-77270.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
